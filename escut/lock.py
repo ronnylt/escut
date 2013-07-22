04e5caaefb61ec2c10aa43bcacfabb78ca018e3e
ronnylt <ronnyherc@gmail.com>
2013-07-22 11:34:01 +0200

    Added lock renew implementation

--- escut/lock.py
+++ escut/lock.py
@@ -19,18 +19,48 @@ class Lock:
 class LockManager:
 
     __redis = None
+    __release_script = None
+    __renew_script = None
 
     def __init__(self, redis):
         self.__redis = redis
+        self.__release_script = self.__register_release_script()
+        self.__renew_script = self.__register_renew_script()
 
     def lock(self, resource, lifetime):
         self.__redis.set("lock:%s" % resource, 1, ex=lifetime, nx=True)
 
     def release(self, resource, token):
-        pass
+        return self.__release_script(keys=["lock:%s" % resource], args=[token])
 
     def renew(self, resource, token, lifetime):
-        pass
+        return self.__renew_script(keys=["lock:%s" % resource], args=[token, lifetime])
 
     def get_lifetime(self, resource):
         return self.__redis.ttl("lock:%s" % resource)
+
+    def __register_release_script(self):
+        script = """
+            if redis.call("GET", KEYS[1]) == ARGV[1] then
+                return redis.call("DEL", KEYS[1])
+            else
+                return 0
+            end
+        """
+        return self.__redis.register_script(script)
+
+    def __register_renew_script(self):
+        script = """
+            local current_value = redis.call("GET", KEYS[1])
+
+            if current_value == ARGV[1] then
+                -- 1 if the timeout was set.
+                -- 0 if key does not exist or the timeout could not be set.
+                return redis.call("EXPIRE", KEYS[1], ARGV[2])
+            elseif not current_value == nil then
+                return 2
+            else
+                return 3
+            end
+        """
+        return self.__redis.register_script(script)
--- escut/lock_handler.py
+++ escut/lock_handler.py
@@ -1,6 +1,7 @@
 
 import tornado.web
 
+
 class LockHandler(tornado.web.RequestHandler):
     @property
     def lock_manager(self):
@@ -18,7 +19,22 @@ class LockHandler(tornado.web.RequestHandler):
             self.set_status(201)
 
     def put(self):
-        pass
+        resource = self.get_argument('resource')
+        lifetime = self.get_argument('lifetime')
+        token = self.get_argument('token')
+
+        result = self.lock_manager.renew(resource, token, lifetime)
+
+        if result == 0:
+            self.send_error(status_code=404, message="lock not found")
+        elif result == 1:
+            self.set_status(200)
+            self.write("ok=>%s" % lifetime )
+        elif result == 2:
+            self.send_error(status_code=403, message="invalid lock token")
+        elif result == 3:
+            self.send_error(status_code=404, message="lock not found")
+
 
     def delete(self, resource):
         lock = self.lock_manager.release(resource)
